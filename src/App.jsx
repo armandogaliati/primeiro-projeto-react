04a56d44fc8ed32935d56a5f97063a0c20b428e4
armandogaliati <armandogaliati@gmail.com>
2024-06-07 14:14:36 -0300

    project start, added navbar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+class App extends React.Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
